a2c5ad2d82dceaafe0e3f73fb63f7029f9a8adce
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2024-09-20 14:42:54 +1000

    final

--- 49005349_A1_B/IDS.py
+++ 49005349_A1_B/IDS.py
@@ -1,10 +1,9 @@
 import sys
-import os
 import scapy.all as scapy
 from datetime import datetime
 
-tcp_packet_timestamps = {}
-tcp_last_alert_count = {}
+timestamps = {}
+alert_count = {}
 
 def log_alert(log_file, message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -70,20 +69,20 @@ def parse_rules(rules_file):
             })
     return rules
 
-def check_detection_filter(packet_time, rule_id, count, seconds):
-    if rule_id not in tcp_packet_timestamps:
-        tcp_packet_timestamps[rule_id] = []
-        tcp_last_alert_count[rule_id] = 0
+def filterPackets(packet_time, rule_id, count, seconds):
+    if rule_id not in timestamps:
+        timestamps[rule_id] = []
+        alert_count[rule_id] = 0
 
-    tcp_packet_timestamps[rule_id].append(packet_time)
-    tcp_packet_timestamps[rule_id] = [t for t in tcp_packet_timestamps[rule_id] if packet_time - t <= seconds]
+    timestamps[rule_id].append(packet_time)
+    timestamps[rule_id] = [t for t in timestamps[rule_id] if packet_time - t <= seconds]
     
-    if len(tcp_packet_timestamps[rule_id]) > count:
-        if tcp_last_alert_count[rule_id] < len(tcp_packet_timestamps[rule_id]) - count:
-            tcp_last_alert_count[rule_id] += 1
+    if len(timestamps[rule_id]) > count:
+        if alert_count[rule_id] < len(timestamps[rule_id]) - count:
+            alert_count[rule_id] += 1
             return True
     else:
-        tcp_last_alert_count[rule_id] = 0
+        alert_count[rule_id] = 0
     
     return False
 
@@ -92,29 +91,24 @@ def apply_rules(packets, rules):
         for packet in packets:
             for rule in rules:
                 if rule['protocol'] == "tcp" and packet.haslayer(scapy.TCP):
-                    src_ip = packet[scapy.IP].src
-                    dst_ip = packet[scapy.IP].dst
-                    if ((src_ip == rule['src_ip'] or rule['src_ip'] == "any") and 
-                        (dst_ip == rule['dst_ip'] or rule['dst_ip'] == "any")):
-                        
-                        rule_matched = True
-                        
+                    if ((packet[scapy.IP].src == rule['src_ip'] or rule['src_ip'] == "any") and (packet[scapy.IP].dst == rule['dst_ip'] or rule['dst_ip'] == "any")):
+                        ruleIsTrue = True
                         if rule.get('flags'):
                             tcp_flags = packet[scapy.TCP].flags
                             if rule['flags'] not in tcp_flags:
-                                rule_matched = False
+                                ruleIsTrue = False
                         
                         if rule.get('content'):
                             if not packet.haslayer(scapy.Raw) or rule['content'] not in packet[scapy.Raw].load.decode(errors='ignore'):
-                                rule_matched = False
+                                ruleIsTrue = False
                         
-                        if rule_matched and rule.get('detection_filter'):
+                        if ruleIsTrue and rule.get('detection_filter'):
                             count = int(rule['detection_filter'][0].split()[1])
                             seconds = int(rule['detection_filter'][1].split()[1])
                             rule_id = f"{rule['src_ip']}_{rule['dst_ip']}_{rule['flags']}_{rule['content']}"
-                            if check_detection_filter(packet.time, rule_id, count, seconds):
+                            if filterPackets(packet.time, rule_id, count, seconds):
                                 log_alert(log_file, rule['msg'])
-                        elif rule_matched:
+                        elif ruleIsTrue:
                             log_alert(log_file, rule['msg'])
                 
                 elif rule['protocol'] == "icmp" and packet.haslayer(scapy.ICMP):
@@ -122,35 +116,20 @@ def apply_rules(packets, rules):
                         log_alert(log_file, rule['msg'])
                 
                 elif rule['protocol'] == "ip" and packet.haslayer(scapy.IP):
-                    if ((packet[scapy.IP].src == rule['src_ip'] or rule['src_ip'] == "any") and 
-                        (packet[scapy.IP].dst == rule['dst_ip'] or rule['dst_ip'] == "any")):
+                    if ((packet[scapy.IP].src == rule['src_ip'] or rule['src_ip'] == "any") and (packet[scapy.IP].dst == rule['dst_ip'] or rule['dst_ip'] == "any")):
                         log_alert(log_file, rule['msg'])
                 
                 elif rule['protocol'] == "udp" and packet.haslayer(scapy.UDP):
-                    if ((packet[scapy.IP].src == rule['src_ip'] or rule['src_ip'] == "any") and 
-                        (packet[scapy.IP].dst == rule['dst_ip'] or rule['dst_ip'] == "any")):
+                    if ((packet[scapy.IP].src == rule['src_ip'] or rule['src_ip'] == "any") and (packet[scapy.IP].dst == rule['dst_ip'] or rule['dst_ip'] == "any")):
                         if rule.get('content'):
                             if packet.haslayer(scapy.Raw) and rule['content'] in packet[scapy.Raw].load.decode(errors='ignore'):
                                 log_alert(log_file, rule['msg'])
-                        elif ((packet[scapy.UDP].sport == int(rule['src_port']) or rule['src_port'] == "any") and
-                              (packet[scapy.UDP].dport == int(rule['dst_port']) or rule['dst_port'] == "any")):
+                        elif ((packet[scapy.UDP].sport == int(rule['src_port']) or rule['src_port'] == "any") and (packet[scapy.UDP].dport == int(rule['dst_port']) or rule['dst_port'] == "any")):
                             log_alert(log_file, rule['msg'])
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python3 IDS.py <path_to_the_pcap_file> <path_to_the_IDS_rules>")
-        sys.exit(1)
-    
     pcap_file = sys.argv[1]
     rules_file = sys.argv[2]
-
-    if not os.path.isfile(pcap_file):
-        print(f"Error: The pcap file '{pcap_file}' does not exist.")
-        sys.exit(1)
-    
-    if not os.path.isfile(rules_file):
-        print(f"Error: The IDS rules file '{rules_file}' does not exist.")
-        sys.exit(1)
     
     packets = parse_pcap(pcap_file)
     rules = parse_rules(rules_file)
